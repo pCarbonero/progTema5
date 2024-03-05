d64b5d8d13e0bcf81214d284a07512ea06f13865
pCarbonero <pablo.carbonero@iesnervion.es>
2024-03-05 21:31:35 +0100

    [upd] un comment ej 03 tema 5

--- Tema5/src/ejercicio03/Main.java
+++ Tema5/src/ejercicio03/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// creamos primer objeto de la clase
 		Punto p1 = new Punto(1,3);
+		// creamos otro objeto de la clase
 		Punto p2 = new Punto(7, 11);
 		
+		// probamos función imprime
 		p1.imprime();
 		
 		p1.setXY(4, 3);
