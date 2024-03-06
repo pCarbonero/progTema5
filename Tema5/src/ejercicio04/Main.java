1c81d0ae9509a5c1a1b07cda73e3ab6f280ba65a
pCarbonero <pablo.carbonero@iesnervion.es>
2024-03-06 09:40:36 +0100

    [new] ejercicio05

---
+++ Tema5/src/ejercicio04/Main.java
@@ -0,0 +1,29 @@
+package ejercicio04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// creamos un objeto de la clas 
+		Articulo art1 = new Articulo("Cort G260SE", 387, 5);
+		
+		//funcion imprime
+		art1.imprimeInfo();
+		
+		// precio con descuento
+		System.out.println("Precio con descuento: " + art1.getPVPDescuento(10));
+		
+		// vender
+		art1.vender(1);
+		
+		//funcion imprime
+		art1.imprimeInfo();
+		
+		// almacenar
+		art1.almacenar(3);
+		
+		//funcion imprime
+		art1.imprimeInfo();
+
+	}
+
+}
